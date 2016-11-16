7ad473b96ed972ad546b75eb33002c841576a189
Rama Milaneh <ramamilaneh@Ramas-MacBook-Air.local>
2016-11-16 18:03:45 -0500

    read/write firebase working

--- CakeLane/ViewController.swift
+++ CakeLane/ViewController.swift
@@ -1,15 +1,24 @@
 //
 //  ViewController.swift
-//  CakeLane
+//  FireBaseTesting
 //
-//  Created by Ian Rahman on 11/16/16.
-//  Copyright © 2016 FlatironSchool. All rights reserved.
+//  Created by Rama Milaneh on 11/14/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+   var databaseReference = FIRDatabase.database().reference()
+    
+    @IBOutlet weak var activityName: UITextField!
+    
+    @IBOutlet weak var activityOwner: UITextField!
 
+    @IBOutlet weak var activityDate: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // Mark: - create an activity on firebase using textfield's information
+    @IBAction func saveButton(_ sender: Any) {
+        
+        guard let unwrappedName = self.activityName.text else {return}
+        let owner = self.activityOwner.text ?? ""
+        let date = self.activityDate.text ?? ""
+        let newActivity = Activity(owner: owner, name: unwrappedName, date: date)
+       let addedActivity = self.databaseReference.child("activities").childByAutoId()
+        addedActivity.setValue(newActivity.toAnyObject())
+       
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CakeLane' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CakeLane
+pod 'Firebase/Database'
+end
